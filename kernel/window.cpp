7e114382ba066ed1f0d4501708ca36caab6b6e48
kinpoko <79001545+kinpoko@users.noreply.github.com>
2021-06-27 00:38:09 +0900

    12f

--- kernel/window.cpp
+++ kernel/window.cpp
@@ -97,14 +97,6 @@ namespace {
     ".$$$$$$$$$$$$$$@",
     "@@@@@@@@@@@@@@@@",
   };
-
-  constexpr PixelColor ToColor(uint32_t c) {
-    return {
-      static_cast<uint8_t>((c >> 16) & 0xff),
-      static_cast<uint8_t>((c >> 8) & 0xff),
-      static_cast<uint8_t>(c & 0xff)
-    };
-  }
 }
 
 void DrawWindow(PixelWriter& writer, const char* title) {
@@ -140,4 +132,20 @@ void DrawWindow(PixelWriter& writer, const char* title) {
             writer.Write({win_w - 5 - kCloseButtonWidth + x, 5 + y}, c);
             }
     }
+}
+
+void DrawTextbox(PixelWriter& writer, Vector2D<int> pos, Vector2D<int> size) {
+  auto fill_rect =
+    [&writer](Vector2D<int> pos, Vector2D<int> size, uint32_t c) {
+      FillRectangle(writer, pos, size, ToColor(c));
+    };
+
+  // fill main box
+  fill_rect(pos + Vector2D<int>{1, 1}, size - Vector2D<int>{2, 2}, 0xffffff);
+
+  // draw border lines
+  fill_rect(pos,                            {size.x, 1}, 0x848484);
+  fill_rect(pos,                            {1, size.y}, 0x848484);
+  fill_rect(pos + Vector2D<int>{0, size.y}, {size.x, 1}, 0xc6c6c6);
+  fill_rect(pos + Vector2D<int>{size.x, 0}, {1, size.y}, 0xc6c6c6);
 }
