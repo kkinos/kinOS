e22e8dcf97bccf183d5ba597cfbd344aa7602a7b
kinpoko <79001545+kinpoko@users.noreply.github.com>
2021-07-01 18:53:50 +0900

    17b

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -35,6 +35,7 @@
 #include "keyboard.hpp"
 #include "task.hpp"
 #include "terminal.hpp"
+#include "fat.hpp"
 
 /*printkを実装する*/
 int printk(const char* format, ...) {
@@ -140,6 +141,7 @@ extern "C" void KernelMainNewStack(
   InitializeMemoryManager(memory_map);
   InitializeInterrupt();
 
+  fat::Initialize(volume_image);
   InitializePCI();
 
   InitializeLayer();
@@ -165,22 +167,6 @@ extern "C" void KernelMainNewStack(
   usb::xhci::Initialize();
   InitializeKeyboard();
   InitializeMouse();
-
-  uint8_t* p = reinterpret_cast<uint8_t*>(volume_image);
-  printk("Volume Image:\n");
-  for (int i = 0; i < 16; ++i) {
-    printk("%04x:", i * 16);
-    for (int j = 0; j < 8; ++j) {
-      printk(" %02x", *p);
-      ++p;
-    }
-    printk(" ");
-    for (int j = 0; j < 8; ++j) {
-      printk(" %02x", *p);
-      ++p;
-    }
-    printk("\n");
-  }
   
   char str[128];
 
--- kernel/terminal.cpp
+++ kernel/terminal.cpp
@@ -5,6 +5,7 @@
 #include "font.hpp"
 #include "layer.hpp"
 #include "pci.hpp"
+#include "fat.hpp"
 
 
 
@@ -132,6 +133,31 @@ void Terminal::ExecuteLine() {
             dev.class_code.base, dev.class_code.sub, dev.class_code.interface);
         Print(s);
         } 
+    } else if (strcmp(command, "ls") == 0) {
+        auto root_dir_entries = fat::GetSectorByCluster<fat::DirectoryEntry>(
+            fat::boot_volume_image->root_cluster);
+        auto entries_per_cluster =
+            fat::boot_volume_image->bytes_per_sector / sizeof(fat::DirectoryEntry)
+            * fat::boot_volume_image->sectors_per_cluster;
+        char base[9], ext[4];
+        char s[64];
+        for (int i = 0; i < entries_per_cluster; ++i) {
+            ReadName(root_dir_entries[i], base, ext);
+            if (base[0] == 0x00) {
+                break;
+            } else if (static_cast<uint8_t>(base[0]) == 0xe5) {
+                continue;
+            } else if (root_dir_entries[i].attr == fat::Attribute::kLongName) {
+                continue;
+            }
+
+            if (ext[0]) {
+                sprintf(s, "%s.%s\n", base, ext);
+            } else {
+                sprintf(s, "%s\n", base);
+            }
+            Print(s);
+        }
     } else if (command[0] != 0) {
         Print("no such command: ");
         Print(command);
