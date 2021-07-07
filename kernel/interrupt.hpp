869b4664d3a363fe8baf4614f8fbd033b8186e1e
kinpoko <79001545+kinpoko@users.noreply.github.com>
2021-07-07 20:04:23 +0900

    21a

--- kernel/interrupt.hpp
+++ kernel/interrupt.hpp
@@ -44,6 +44,8 @@ constexpr InterruptDescriptorAttribute MakeIDTAttr(
   return attr;
 }
 
+const int kISTForTimer = 1;
+
 void SetIDTEntry(InterruptDescriptor& desc,
                  InterruptDescriptorAttribute attr,
                  uint64_t offset,
