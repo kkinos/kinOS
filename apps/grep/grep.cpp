d3943f56777fcaf21e1f6bd5855979167b3bbafc
kinpoko <79001545+kinpoko@users.noreply.github.com>
2021-12-09 11:11:30 +0900

    Add apps/sort

--- apps/grep/grep.cpp
+++ apps/grep/grep.cpp
@@ -3,25 +3,25 @@
 #include <regex>
 
 extern "C" void main(int argc, char** argv) {
-  if (argc < 3) {
-    printf("Usage: %s <pattern> <file>\n", argv[0]);
-    exit(1);
-  }
-
-  std::regex pattern{argv[1]};
-
-  FILE* fp = fopen(argv[2], "r");
-  if (fp == nullptr) {
-    printf("failed to open: %s\n", argv[2]);
-    exit(1);
-  }
-
-  char line[256];
-  while (fgets(line, sizeof(line), fp)) {
-    std::cmatch m;
-    if (std::regex_search(line, m, pattern)) {
-      printf("%s", line);
+    if (argc < 2) {
+        printf("Usage: %s <pattern> <file>\n", argv[0]);
+        exit(1);
     }
-  }
-  exit(0);
+
+    std::regex pattern{argv[1]};
+
+    FILE* fp = stdin;
+    if (argc >= 3 && (fp = fopen(argv[2], "r")) == nullptr) {
+        fprintf(stderr, "failed to open: %s\n", argv[2]);
+        exit(1);
+    }
+
+    char line[256];
+    while (fgets(line, sizeof(line), fp)) {
+        std::cmatch m;
+        if (std::regex_search(line, m, pattern)) {
+            printf("%s", line);
+        }
+    }
+    exit(0);
 }
